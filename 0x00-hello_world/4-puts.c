8bca9c04ec38e4770f809adc7aef14db5612a820
ostechGroup <okechukwuofoegbu68@gmail.com>
2023-02-19 10:19:31 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - prints "programming is like building a multilingual puzzle, followed by a new line.
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
  *
- * * return: Always o. 
+ * Return: Always 0.
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
+	return (0);
 }
