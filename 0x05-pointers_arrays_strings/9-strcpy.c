b04953770cd8299a26f181a0e8b6b3e8e23bac95
ostechGroup <okechukwuofoegbu68@gmail.com>
2023-03-06 06:56:14 -0800

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - copy strings from first array to second
+ * @dest: carries the first array
+ * @src: represents second array
+ * Return: return output of the copied string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+char *s = strcpy(dest, src);
+return (s);
+}
