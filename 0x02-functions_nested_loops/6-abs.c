e93ee7f89d1c4517d8c87274afacd40ecea2ec2f
ostechGroup <okechukwuofoegbu68@gmail.com>
2023-03-06 17:32:57 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *_abs - returns the abs value of an argument
+ *@ab: carries argument
+ *Return: value of a
+ */
+
+int _abs(int ab)
+{
+int a = abs(ab);
+return (a);
+}
