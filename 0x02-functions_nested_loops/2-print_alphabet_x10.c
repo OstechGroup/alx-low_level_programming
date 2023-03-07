9024a632c602d3c6d171101b3d74fc6b661e75c1
ostechGroup <okechukwuofoegbu68@gmail.com>
2023-03-06 17:19:32 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+
+/**
+ * print_alphabet_x10 - prints 10 rows of a-z
+ *
+ * Return - nothing always
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char a;
+for (i = 0; i <= 9; i++)
+{
+for (a = 'a'; a <= 'z'; a++)
+{
+_putchar (a);
+}
+_putchar ('\n');
+}
+}
