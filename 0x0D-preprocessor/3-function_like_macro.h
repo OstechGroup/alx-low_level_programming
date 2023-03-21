f698992a6fe69511455f875ebe3e8c7d6eae8c69
ostechGroup <okechukwuofoegbu68@gmail.com>
2023-03-21 11:01:09 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * File - 3-function_like_macro.h
+ * Desc: defines a micro that computes the absolute value of a given number
+ * @X: parameter passed to macro
+ */
+
+#define ABS(X) ((X) < 0 ? ((X) * -1) : (X))
+
+#endif
