1944c2a9d1f447eab81a3ca4edc17f733c527801
ostechGroup <okechukwuofoegbu68@gmail.com>
2023-03-21 11:03:06 -0700

    4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File: 4-sum.h
+ * Desc: contains a micro that returns the sum of 2 numbers
+ * @X: buffer for first number
+ * @Y: buffer for second number
+ */
+
+#define SUM(X, Y) ((X) + (Y))
+
+#endif
